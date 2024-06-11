abe8b988c1053af74163ba335ba035e37280b9cb
정우영 <101693311+wooyoungman@users.noreply.github.com>
2024-06-11 16:55:46 +0900

    update : EC2 개인정보 필터링

--- NodeJS Server/db/index.js
+++ NodeJS Server/db/index.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: '13.124.218.110',
+  host: 'xx.xx.xx.xxx', // id
   user: 'wooyoung',
-  password: "1234",
+  password: "xxxx", // pwd
   database: 'wooDB',
   waitForConnections: true,
   connectionLimit: 10,
